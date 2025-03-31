10d835d163bb14a70e4e0264f4ecf537f5533fbb
IslamRtR <nargis1788@icloud.com>
2025-03-31 19:56:55 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-import BookCard from './components/BookCard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import FeedbackForm from './components/FeedbackForm'
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <div className="cards">
-      <BookCard image="https://media.istockphoto.com/id/153952958/photo/blank-hardcover-book-with-copy-space.jpg?s=612x612&w=0&k=20&c=0GwreFm2LXIR0_irlkOvvz45hfRlova_pTZr-v32Pyg=" title="red book" authors1="Ислам" authors2="2008"/>
-      <BookCard image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSlf4l_FWwTkgsxIoMEJkkqeAvznJnEuk0jg&s" title="blue book" authors1="Бибарыс" authors2="2010"/>
-      <BookCard image="https://www.shutterstock.com/image-photo/closed-green-book-isolated-on-260nw-265051619.jpg" title="green book" authors1="Еркебулан" authors2="2009"/>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<FeedbackForm/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App;
